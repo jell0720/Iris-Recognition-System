c0d1b037c0fc5f9bfb428f839e128b49a61ca8c7
AntiAegis <thuypn9a4@gmail.com>
2017-09-18 22:33:22 +0700

    @antiaegis: Sample database.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Iris-Recognition-System
-An Iris recognition system implemented on a system including Raspberry Pi and FPGA
+
+
+## Introduction
+This is an Iris recognition engine implemented on a system including Raspberry Pi and FPGA.
+
+
+## Project structure
+|
+|--> fpga 			: Program runs on hardware.
+|
+|--> iris-matlab 	: Model the system on Matlab form.
+|
+|--> iris-python 	: Program runs on Linux platform.
+|
+|--> linux 			: Linux platform and database cloud.
+
--- iris-python/ws.py
+++ iris-python/ws.py
@@ -1,18 +1,26 @@
 ##
-##	[1]. Create workspace for the project.
-##	Author: Nguyen Chinh Thuy.
-##	Date  : 17/08/2017.
+##  Author          :   Nguyen Chinh Thuy.
+##  Date            :   18/09/2017.
+##  
+##  Interpreter     :   Python 3.5
+##  IDE             :   Pycharm Community 2017.2.1
+##
+##  Description     :   Read iris image.
+##
+##  Input           :   
+##  Output          :
 ##
 
 
-##
+##-----------------------------------------------------------------------------
 ## Import
-##
+##-----------------------------------------------------------------------------
 import cv2
+import numpy
 from matplotlib.pyplot import *
 
 
-##
+##-----------------------------------------------------------------------------
 ## Function : Read eye image.
 ##
 ## Input    : fname : Name of file eye image.
@@ -20,14 +28,14 @@ from matplotlib.pyplot import *
 ##
 ## Output   : im    : Read image.
 ##            imsz  : Size of the image.
-##
+##-----------------------------------------------------------------------------
 def readim(fname, dbpath):
 	im = cv2.imread(dbpath + fname, 0)
 	imsz = [im.shape[0], im.shape[1]]
 	return im, imsz
 
 
-##
+##-----------------------------------------------------------------------------
 ## Function : Plot read image.
 ##
 ## Input    : im    : Read image.
@@ -35,7 +43,7 @@ def readim(fname, dbpath):
 ##            fname : Name of file eye image.
 ##
 ## Output   : None.
-##
+##-----------------------------------------------------------------------------
 def vs(im, imsz, fname):
 	figure(1)
 	title('[%s].Input image [%dx%d]' % (fname[0:8],imsz[0],imsz[1]))
